e31f51479b9cc23840041052fe76fa0ae7a11f43
Sean <2136seanpaul@gmail.com>
2024-12-17 22:23:03 +0800

    Add Login

--- routes/User/auth.js
+++ routes/User/auth.js
@@ -5,4 +5,7 @@ const authController = require("../../controllers/User/AuthController");
 // Sign up
 router.post("/signup", authController.post_sign_up);
 
+// Login
+router.post("/login", authController.post_login);
+
 module.exports = router;

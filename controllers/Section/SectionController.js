e7a33d8ce1b7cfbf65893179fd3433f44a2c69ae
Sean <2136seanpaul@gmail.com>
2025-02-12 13:39:19 +0800

    Add Section Enroll Route

--- controllers/Section/SectionController.js
+++ controllers/Section/SectionController.js
@@ -2,6 +2,12 @@ const Section = require("../../models/User/Section");
 const User = require("../../models/User/User");
 const AppError = require("../../utilities/appError");
 const catchAsync = require("../../utilities/catchAsync");
+const crypto = require("crypto");
+
+const generateCode = (length) => {
+  // Generate Verification Token
+  return crypto.randomBytes(length / 2).toString("hex");
+};
 
 // Get Section by Id
 const section_get = catchAsync(async (req, res, next) => {
@@ -22,11 +28,12 @@ const section_get = catchAsync(async (req, res, next) => {
 // Create Section
 const section_post = catchAsync(async (req, res, next) => {
   const { userId } = req.query;
-
   const isUserValid = await User.findById(userId);
 
   if (!isUserValid)
     return next(new AppError("User not found. Invalid User ID.", 404));
+
+  req.body.sectionCode = generateCode(6).toUpperCase();
 
   const newSection = new Section({
     ...req.body,
@@ -109,9 +116,69 @@ const section_delete = catchAsync(async (req, res, next) => {
     .json({ message: "Section Successfully Deleted", deletedSection });
 });
 
+// Section Enroll
+const section_enroll = catchAsync(async (req, res, next) => {
+  const { id, sectionCode, enroll, unenroll } = req.query;
+  const { userId } = req.body;
+
+  if (!sectionCode && !id)
+    return next(
+      new AppError(
+        "Section identifier not found. Invalid Request Parameters.",
+        400
+      )
+    );
+  if (!enroll && !unenroll)
+    return next(new AppError("Invalid Request Parameters.", 400));
+  if (!userId)
+    return next(
+      new AppError("User ID not found. Invalid Request Parameters.", 400)
+    );
+
+  let section;
+
+  if (id) {
+    section = await Section.findById(id);
+  } else {
+    section = await Section.findOne({
+      sectionCode: sectionCode.toUpperCase(),
+    });
+  }
+
+  if (!section)
+    return next(new AppError("Section not found. Invalid Section Code.", 404));
+
+  let updatedSection;
+
+  if (enroll) {
+    updatedSection = await Section.findByIdAndUpdate(
+      section._id,
+      { $push: { students: userId } },
+      { new: true }
+    );
+  } else {
+    updatedSection = await Section.findByIdAndUpdate(
+      section._id,
+      { $pull: { students: userId } },
+      { new: true }
+    );
+  }
+
+  if (!updatedSection) {
+    return next(
+      new AppError("Section not found. Section Update Not Successful.", 404)
+    );
+  }
+
+  return res
+    .status(200)
+    .json({ message: "Section Updated Successfully", updatedSection });
+});
+
 module.exports = {
   section_get,
   section_post,
   section_put,
+  section_enroll,
   section_delete,
 };

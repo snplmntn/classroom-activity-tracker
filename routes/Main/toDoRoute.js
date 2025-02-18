43b0e131d2a6908edd3e895729ec98c96da39c0a
Sean <2136seanpaul@gmail.com>
2025-02-18 12:39:33 +0800

    To Do Route: Restrict Students from creating To Dos'

--- routes/Main/toDoRoute.js
+++ routes/Main/toDoRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ToDoController = require("../../controllers/Main/ToDoController");
+const checkRole = require("../../utilities/checkRole");
 
 // Get To Do
 router.get("/", ToDoController.toDo_get);
@@ -9,7 +10,11 @@ router.get("/", ToDoController.toDo_get);
 router.get("/u", ToDoController.toDo_userGet);
 
 // Create To Do
-router.post("/", ToDoController.toDo_post);
+router.post(
+  "/",
+  checkRole("officer", "beadle", "admin"),
+  ToDoController.toDo_post
+);
 
 // Update To Do
 router.put("/", ToDoController.toDo_put);
